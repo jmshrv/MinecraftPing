d03e4d0f5d7d23bd996d273f159694d5c17f9cd5
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-27 19:52:15 +0100

    MinecraftConnection scaffolding

--- Sources/MinecraftPing/MinecraftPing.swift
+++ Sources/MinecraftPing/MinecraftPing.swift
@@ -1,2 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+enum MinecraftConnectionError: Error {
+    case connectionFailed(Error)
+    case invalidResponse
+}
+
+protocol MinecraftPacketContent {
+    var packetId: Int32 { get }
+}
+
+struct MinecraftPacket<T: MinecraftPacketContent> {
+    var length: Int32
+    let data: T
+}
+
+
+/// A connection to a Minecraft server
+public struct MinecraftConnection {
+    let connection: URLSessionStreamTask
+    
+    public init(hostname: String, port: Int) {
+        connection = URLSession.shared.streamTask(withHostName: hostname, port: port)
+        connection.resume()
+    }
+    
+    public func ping() async throws {
+        
+    }
+}
---
+++ Sources/MinecraftPing/VarInt.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by James on 27/06/2024.
+//
+
+import Foundation
+
+fileprivate let SEGMENT_BITS: UInt32 = 0x7F
+fileprivate let CONTINUE_BIT: UInt32 = 0x80
+
+extension Int32 {
+    var varInt: Data {
+        var value = UInt32(bitPattern: self)
+        var output = Data()
+        
+        while true {
+            if value & ~SEGMENT_BITS == 0 {
+                output.append(UInt8(value))
+                return output
+            }
+            
+            output.append(UInt8(value & SEGMENT_BITS | CONTINUE_BIT))
+            
+            value >>= 7
+        }
+    }
+}
