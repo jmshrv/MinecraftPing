8c32b73337389be2a8526db43bf4661662e56e6b
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-28 00:46:14 +0100

    Ping command working!

--- Sources/MinecraftPing/MinecraftPing.swift
+++ Sources/MinecraftPing/MinecraftPing.swift
@@ -5,13 +5,76 @@ enum MinecraftConnectionError: Error {
     case invalidResponse
 }
 
-protocol MinecraftPacketContent {
+protocol MinecraftEncodable {
+    var minecraftEncoded: Data { get }
+}
+
+extension String: MinecraftEncodable {
+    var minecraftEncoded: Data {
+        var out = Data()
+        
+        let stringBytes = self.utf8
+        
+        out.append(Int32(stringBytes.count).varInt)
+        out.append(Data(stringBytes))
+        
+        return out
+    }
+}
+
+protocol MinecraftPacketContent: MinecraftEncodable {
     var packetId: Int32 { get }
 }
 
-struct MinecraftPacket<T: MinecraftPacketContent> {
-    var length: Int32
+struct MinecraftPacket<T: MinecraftPacketContent>: MinecraftEncodable {
+    var minecraftEncoded: Data {
+        var out = Data()
+        
+        out.append(length.varInt)
+        out.append(data.minecraftEncoded)
+        
+        return out
+    }
+    
+    var length: Int32 {
+        Int32(data.minecraftEncoded.count)
+    }
+    
     let data: T
+}
+
+struct MinecraftHandshake: MinecraftPacketContent {
+    var minecraftEncoded: Data {
+        var out = Data()
+        
+        out.append(packetId.varInt)
+        out.append(protocolVersion.varInt)
+        out.append(serverAddress.minecraftEncoded)
+        
+        withUnsafeBytes(of: serverPort.bigEndian) { buffer in
+            out.append(contentsOf: buffer)
+        }
+        
+        out.append(nextState.varInt)
+        
+        return out
+    }
+    
+    var packetId: Int32 = 0x00
+    let protocolVersion: Int32 = -1
+    
+    let serverAddress: String
+    let serverPort: UInt16
+    
+    let nextState: Int32 = 1
+}
+
+struct MinecraftStatusRequest: MinecraftPacketContent {
+    var minecraftEncoded: Data {
+        packetId.varInt
+    }
+    
+    var packetId: Int32 = 0x00
 }
 
 
@@ -19,12 +82,26 @@ struct MinecraftPacket<T: MinecraftPacketContent> {
 public struct MinecraftConnection {
     let connection: URLSessionStreamTask
     
-    public init(hostname: String, port: Int) {
-        connection = URLSession.shared.streamTask(withHostName: hostname, port: port)
+    let hostname: String
+    let port: UInt16
+    
+    public init(hostname: String, port: UInt16) {
+        self.hostname = hostname
+        self.port = port
+        
+        connection = URLSession.shared.streamTask(withHostName: hostname, port: Int(port))
         connection.resume()
     }
     
     public func ping() async throws {
+        let handshake = MinecraftPacket(data: MinecraftHandshake(serverAddress: hostname, serverPort: port))
+        try await connection.write(handshake.minecraftEncoded, timeout: .zero)
         
+        let statusRequest = MinecraftPacket(data: MinecraftStatusRequest())
+        try await connection.write(statusRequest.minecraftEncoded, timeout: .zero)
+        
+        let response = try await connection.readData(ofMinLength: 0, maxLength: .max, timeout: .zero)
+        
+        print(response.0?.base64EncodedString() ?? "None")
     }
 }
--- Tests/MinecraftPingTests/MinecraftPingTests.swift
+++ Tests/MinecraftPingTests/MinecraftPingTests.swift
@@ -2,11 +2,9 @@ import XCTest
 @testable import MinecraftPing
 
 final class MinecraftPingTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testPing() async throws {
+        let connection = MinecraftConnection(hostname: "localhost", port: 25565)
+        
+        try await connection.ping()
     }
 }
